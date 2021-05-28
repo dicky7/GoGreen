19c9546667577f8f3ea395604b4b8b4aa1ee532c
Dicky7 <dickyangga73@gmail.com>
2021-05-28 18:01:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/gogreen/ui/splashScreen/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.example.gogreen.ui.splashScreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.gogreen.MainActivity
+import com.example.gogreen.R
+import com.example.gogreen.ui.login.ui.login.LoginActivity
+
+class SplashActivity : AppCompatActivity() {
+    companion object {
+        private const val TWO_SECOND_IN_MILLIS = 5000L
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, TWO_SECOND_IN_MILLIS)
+    }
+}
