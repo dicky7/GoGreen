993ed46d5a7d05fb68d5373ac6af79468ce0f0a3
Imam Yusril Alawi <alawi.id05@gmail.com>
2021-06-02 01:49:26 +0700

    Add ApiClassification.kt

---
+++ app/src/main/java/com/manut/gogreen/data/source/remote/network/classification/ApiClassification.kt
@@ -0,0 +1,26 @@
+package com.manut.gogreen.data.source.remote.network.classification
+
+import com.manut.gogreen.data.source.remote.network.ApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+class ApiClassification {
+    companion object{
+        fun getApiService(): ApiService {
+            val loggingInterceptor =
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build()
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://ml-api-rupnuawd4a-et.a.run.app/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+            return retrofit.create(ApiService::class.java)
+        }
+    }
+}
